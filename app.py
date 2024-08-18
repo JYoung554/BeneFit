bc30fd7be6fdee9b9e153a1641cfcd4cea029c11
JYoung554 <jontefears10@gmail.com>
2024-08-17 20:29:38 -0400

    setup app.py server and database

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_cors import CORS
+
+import os
+from models.db import db
+
+
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app)
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if (DATABASE_URL):
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL.replace('://', 'ql://', 1)
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.env = 'production'
+else:
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/benefit_development'
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
