08f4315b63c6fe4b714b49c523878ee52719759d
JYoung554 <jontefears10@gmail.com>
2024-08-18 20:56:47 -0400

    created workout.py model

---
+++ models/workout.py
@@ -0,0 +1,21 @@
+from models.db import db
+from datetime import datetime
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+    id = db.Column(db.Integer, primary_key=True)
+    muscle_group = db.Column(db.String(50), nullable=True)
+    plan_id = db.relationship("Plan", backref=db.backref("plan_workouts"), lazy=True)
+    date_created = db.Column(db.datetime(default=datetime.now), nullable=False)
+
+    def __init__(self, muscle_group):
+        self.muscle_group = muscle_group
+
+    def json(self):
+        return {'muscle_group': self.muscle_group}
+    
+    def create(self):
+        db.add(self)
+        db.session.commit()
+        return self.json()
+        
