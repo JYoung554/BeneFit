4ef045d33a7f8a51461b5f40bd3cd5193203f34f
JYoung554 <jontefears10@gmail.com>
2024-08-29 12:17:58 -0400

    imported user actions to Register.jsx

--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -1,7 +1,11 @@
 import { React } from 'react'
 import { connect } from 'react-redux'
 import RegisterForm from '../forms/RegisterForm'
-import { SetAuthenticated } from '../../store/actions/AuthActions'
+import {
+  SetAuthenticated,
+  createUser,
+  addUser
+} from '../../store/actions/AuthActions'
 
 const mapStateToProps = ({ authState }) => {
   return { authState }
